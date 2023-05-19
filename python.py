193f267f622bba76a3ad268a3a664b0706d7ab8d
Rafaelmoura06 <127312275+Rafaelmoura06@users.noreply.github.com>
2023-05-19 11:51:05 -0300

    Add files via upload

---
+++ python.py
@@ -0,0 +1,16 @@
+print("Mensagem que que eu quero")
+print ('Mensagem 2')
+print ('Fulano é meu "amigo"')
+print("Fulano \n é seu \"amigo\"")
+print(1+1)
+print("1 x 1 =", 1*1)
+print("10³ =", 10**3)
+print("10³ =",10**3)
+print("10³=", pow (10,3) )
+
+nome = ""
+print(nome)
+nome = 1
+
+
+
